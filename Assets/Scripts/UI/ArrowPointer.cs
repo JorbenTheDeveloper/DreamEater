f6c58a8453b6a424c812604106c8eb51e0309284
JorbenTheDeveloper <134649421+JorbenTheDeveloper@users.noreply.github.com>
2024-05-06 14:47:09 -0700

    little changes

--- Assets/Scripts/UI/ArrowPointer.cs
+++ Assets/Scripts/UI/ArrowPointer.cs
@@ -24,18 +24,7 @@ public class ArrowPointer : MonoBehaviour
 
 	void Update()
     {
-        /*        // Update the current angle based on the orbit speed and time
-                currentAngle += orbitSpeed * Time.deltaTime;
-                currentAngle %= 360; // Keep the angle in the range of 0 to 360
-
-                // Calculate the new position based on the current angle
-                float radian = currentAngle * Mathf.Deg2Rad; // Convert degrees to radians
-                Vector3 orbitPosition = new Vector3(Mathf.Cos(radian), Mathf.Sin(radian), 0) * orbitRadius;
-                arrow.transform.position = player.position + orbitPosition;
-
-                // Set the arrow rotation to face outward from the center (player)
-                arrow.transform.right = orbitPosition.normalized;*/
-
+        
         FindClosestEatable();
 
         if (currentEatable != null)
